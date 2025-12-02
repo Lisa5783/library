49b2a90c11140bd646a71f666ae7fd491afaa3b3
Bazilik757 <159037883+Bazilik757@users.noreply.github.com>
2025-12-02 10:13:23 +0300

    Update __init__.py

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,15 @@
-from flask import Blueprint
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-bp = Blueprint('main', __name__)
+db = SQLAlchemy()
 
-from app.main import views
+def create_app(config_name="default"):
+    app = Flask(__name__)
+    # конфиг сюда
+    db.init_app(app)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
+
