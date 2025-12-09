f33ac2d6d40691ced9aad440fafa7694900d0233
Lisa5783 <saeveranna@gmail.commm>
2025-12-09 22:26:38 +0000

    Use venv sqlfluff in CI

--- scripts/check_sql.sh
+++ scripts/check_sql.sh
@@ -3,6 +3,5 @@ set -e
 
 echo "Running SQL security & lint check..."
 
-# если у тебя Postgres:
-sqlfluff lint db_schema/*.sql --dialect postgres
-
+# используем sqlfluff из venv, который мы поставили в шаге Install Sqlfluff
+/opt/sqlfluff-venv/bin/sqlfluff lint db_schema/*.sql --dialect postgres
